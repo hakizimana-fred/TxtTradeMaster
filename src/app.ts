a18bfd372a7484e892a72d9c63f2282331d441f9
Hakizimana Fred <hakifred20@gmail.com>
2023-05-16 00:32:34 +0300

    http || https server initialization

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,27 @@
 import express from "express";
+import http from "http";
+import https from "https";
+import fs from "fs";
+import "dotenv/config";
 
-const run = () => {};
+// enable https
+let isHttps = true;
+
+const options = {
+  key: fs.readFileSync("key.pem"),
+  cert: fs.readFileSync("cert.pem"),
+};
+
+const run = () => {
+  const app = express();
+
+  if (isHttps)
+    return https
+      .createServer(options, app)
+      .listen(process.env.PORT, () => console.log("server running on https"));
+  return http
+    .createServer(app)
+    .listen(process.env.PORT, () => console.log("server running on http"));
+};
 
 void run();
